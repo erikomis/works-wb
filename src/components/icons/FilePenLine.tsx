8acef728fbe6b55e8978f58361356a313636e428
erik-martins <erik.ozzetti.martins@gmail.com>
2024-11-01 12:03:50 -0300

    feat: icons

---
+++ src/components/icons/FilePenLine.tsx
@@ -0,0 +1,24 @@
+import { SVGProps } from "react";
+
+export function LucideFilePenLine(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="2em"
+      height="2em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      >
+        <path d="m18 5l-2.414-2.414A2 2 0 0 0 14.172 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2"></path>
+        <path d="M21.378 12.626a1 1 0 0 0-3.004-3.004l-4.01 4.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506zM8 18h1"></path>
+      </g>
+    </svg>
+  );
+}
