2159daca6a563ba2d11a4b2b9907c151a0b36c62
erik-martins <erik.ozzetti.martins@gmail.com>
2024-11-01 12:20:29 -0300

    feat:  app css

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />;
+      <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
