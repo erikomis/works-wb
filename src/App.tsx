22e0a4aa6cbc5fcdbe2994074f77c134ec1ff8fb
erik-martins <erik.ozzetti.martins@gmail.com>
2024-11-01 01:29:07 -0300

    feat:  routes and use react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { router } from "./routes";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />;
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
